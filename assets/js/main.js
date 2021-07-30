a4cc801969a0491c6e814164384f3c059b39f8cc
Maria Rushkova <maryrushkova@gmail.com>
2021-07-30 18:00:52 +0200

    Add smooth scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,31 @@
 "use strict";
 
-document.querySelectorAll(".nav__link").forEach((link) =>
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const id = e.target.getAttribute("data-href");
-    document.querySelector(id).scrollIntoView({
-      behavior: "smooth",
-    });
-  })
-);
-
 const burger = document.querySelector(".burger");
 const burgerOpen = document.querySelector(".burger__button--open");
 const burgerClose = document.querySelector(".burger__button--close");
 const menuMobile = document.querySelector(".nav-mobile");
+const menuLinks = document.querySelectorAll(".nav__link");
 const overlay = document.querySelector(".overlay");
 
-burger.addEventListener("click", function (e) {
-  e.preventDefault();
+const closeMobileMenu = () => {
   burgerOpen.classList.toggle("hidden");
   burgerClose.classList.toggle("hidden");
   menuMobile.classList.toggle("hidden");
   overlay.classList.toggle("hidden");
+};
+
+burger.addEventListener("click", function (e) {
+  e.preventDefault();
+  closeMobileMenu();
+});
+
+menuLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    closeMobileMenu();
+    const id = e.target.getAttribute("href").slice(1);
+    console.log(document.querySelector(id));
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
 });
